0e6ad8d8f4e8eba02fb47f6c82ff9a3d2b770c89
Yuvraj Singh <yuvrajsingh760018@gmail.com>
2026-09-15 22:57:55 +0530

    just start homepage work

--- src/landingpage/home/Hero.js
+++ src/landingpage/home/Hero.js
@@ -1,9 +1,30 @@
 import React from 'react';
+
 function Hero() {
+
     return ( 
-        <><h1><i class="fa-solid fa-camera"></i></h1>
-       <h1><i class="fa-solid fa-user"></i></h1></>
-     );
+        <div className='container p-5'>
+
+            <div className='row text-center'>
+
+                <img 
+                    src='media/image/homeHero.png' 
+                    alt='Hero Image' 
+                    className='mb-5'
+                />
+
+                <h1 className="mt-5">Invest in everything</h1>
+
+                <p>
+                    Online platform to invest in stocks, derivatives, mutual funds, and more
+                </p>
+
+                <button className = 'p-2 btn btn-primary fs-5'style={{width:"20%",margin:"0 auto"}}>Signup Now</button>
+
+            </div>
+
+        </div>
+    );
 }
 
 export default Hero;
